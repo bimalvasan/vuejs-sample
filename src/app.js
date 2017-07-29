98b17966461d8fb55aaf8133892138c3db5eb954
bimalvasan <bimalvasan@gmail.com>
2017-07-29 13:39:05 +0530

    Styles

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 var growler = new Vue({
     el: '#growler',
     data: {
-
+        appName: 'Growler',
+        accentColor: 'accent-color',
+        headers: 'headers',
+        // accentColor: {
+        //     color: '#FF6A00'
+        // },
+        // headers: {
+        //     fontFamily: 'Verdana',
+        //     margin: 0
+        // },
+        // appStyle: {
+        //     color: '',
+        //     fontFamily: 'Verdana',
+        //     margin: 0
+        // },
+        query: ''
     }
 });
